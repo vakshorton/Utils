960f24760acdf50c727448e5a77744d4919d2ed5
vvaks <vvaks@hortonworks.com>
2016-11-28 21:13:23 -0500

    Data Plane client initial commit

---
+++ DATA_PLANE_CLIENT/package/scripts/client.py
@@ -0,0 +1,48 @@
+import sys, os, pwd, signal, time, shutil
+from subprocess import *
+from resource_management import *
+
+class DataPlaneClient(Script):
+  def install(self, env):
+    self.configure(env)
+    import params
+    
+    #debug info
+    Execute('echo list of config dump: ' + str(', '.join(params.list_of_configs)))    
+    Execute('echo master config dump: ' + str(', '.join(params.master_configs)))
+    Execute('echo java home: ' + params.jdk64_home)
+    Execute('echo ambari host: ' + params.ambari_server_host) 
+    Execute('echo ambari port: ' + params.ambari_port)
+    Execute('echo namenode host: ' + params.namenode_host)
+    Execute('echo namenode port: ' + params.namenode_port)
+    Execute('echo hive metastore uri: ' + params.hive_metastore_uri)
+    Execute('echo hbase zookeeper: ' + params.hbase_zookeeper)
+    Execute('echo kafka host: ' + params.kafka_broker_host)
+    Execute('echo kafka port: ' + params.kafka_port)
+    Execute('echo stack_version: ' + params.stack_version_unformatted)
+    
+    Execute('echo Download Data Plane Util Bits')
+    if not os.path.exists(params.install_dir):  
+        os.makedirs(params.install_dir)
+    os.chdir(params.install_dir)
+    Execute('git clone ' + params.download_url)
+    os.chdir(params.install_dir+'/Util')
+    
+    Execute('echo Install the config client')
+    src_dir = ('/var/lib/ambari-agent/cache/stacks/HDP/2.5/services/RANGER_HIVE_PLUGIN/package/configuration')
+    dest_dir = ('/usr/hdp/current/hive-server2/conf/conf.server')
+    src_files = os.listdir(src_dir)
+    for file_name in src_files:
+        full_file_name = os.path.join(src_dir, file_name)
+        if (os.path.isfile(full_file_name)):
+            #shutil.copy(full_file_name, dest_dir)
+      
+  def status(self, env):
+    raise ClientComponentHasNoStatus()
+    
+  def configure(self, env):
+    import params
+    env.set_params(params)
+
+if __name__ == "__main__":
+  DataPlaneClient().execute()
---
+++ DATA_PLANE_CLIENT/package/scripts/params.py
@@ -0,0 +1,39 @@
+
+#!/usr/bin/env python
+from resource_management import *
+
+# server configurations
+config = Script.get_config()
+
+install_dir = config['configurations']['data-plane-config']['install_dir']
+download_url = config['configurations']['data-plane-config']['download_url']
+data_plane_ambari_host = config['configurations']['data-plane-config']['data_plane_ambari_host']
+
+master_configs = config['clusterHostInfo']
+ambari_host = str(master_configs['ambari_server_host'][0])
+
+stack_version_unformatted = config['hostLevelParams']['stack_version']
+
+master_configs = config['clusterHostInfo']
+ambari_server_host = str(master_configs['ambari_server_host'][0])
+namenode_host =  str(master_configs['namenode_host'][0])
+namenode_port = get_port_from_url(config['configurations']['core-site']['fs.defaultFS'])
+hive_metastore_host = str(master_configs['hive_metastore_host'][0])
+hive_metastore_port = get_port_from_url(config['configurations']['hive-site']['hive.metastore.uris'])
+hive_metastore_uri = 'thrift://'+hive_metastore_host+':'+hive_metastore_port
+hbase_zookeeper = config['configurations']['hbase-site']['hbase.zookeeper.quorum']
+atlas_server_host = str(master_configs['atlas_server_host'][0])
+kafka_broker_host = str(master_configs['kafka_broker_hosts'][0])
+if 'port' in config['configurations']['kafka-broker']:
+  kafka_port = str(config['configurations']['kafka-broker']['port'])
+else:
+  kafka_port = get_port_from_url(config['configurations']['kafka-broker']['listeners'])
+
+list_of_configs = config['configurations']
+jdk64_home=config['hostLevelParams']['java_home']
+
+def get_port_from_url(address):
+  if not is_empty(address):
+    return address.split(':')[-1]
+  else:
+    return address
