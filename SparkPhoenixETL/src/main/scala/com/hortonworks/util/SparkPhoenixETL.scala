b0699ba98904c854b967dcf21f3d0e5ea9142c71
vvaks <vvaks@hortonworks.com>
2016-12-07 12:03:06 -0500

    update

--- SparkPhoenixETL/src/main/scala/com/hortonworks/util/SparkPhoenixETL.scala
+++ SparkPhoenixETL/src/main/scala/com/hortonworks/util/SparkPhoenixETL.scala
@@ -34,7 +34,7 @@ object SparkPhoenixETL {
 
     sqlContext.sql("CREATE TABLE IF NOT EXISTS retail_transaction_history (transactionId String,locationId String, item String, accountNumber String, amount Double, currency String, isCardPresent String, ipAddress String, transactionTimeStamp String) PARTITIONED BY (accountType String, shipToState String) CLUSTERED BY (accountNumber) INTO 30 BUCKETS STORED AS ORC")
     sqlContext.sql("CREATE TABLE IF NOT EXISTS retail_products (productId String, productCategory String, manufacturer String, productName String, price Double) PARTITIONED BY (productSubCategory String) CLUSTERED BY (manufacturer) INTO 30 BUCKETS STORED AS ORC")
-    sqlContext.sql("insert overwrite table retail_transaction_history partition(accountType,shipToState) select a.transactionId, locationId, productId as item, accountNumber,amount,currency,isCardPresent,transactionTimeStamp,accountType,shipToState from phoenix_retail_transactions as a, phoenix_retail_item_transactions as b where a.transactionId = b.transactionId")
-    sqlContext.sql("insert overwrite table retail_products partition(productSubCategory) select productId, productCategory,  manufacturer, productName, price, productSubCategory from phoenix_retail_products")
+    sqlContext.sql("insert into table retail_transaction_history partition(accountType,shipToState) select a.transactionId, locationId, productId as item, accountNumber,amount,currency,isCardPresent,transactionTimeStamp,accountType,shipToState from phoenix_retail_transactions as a, phoenix_retail_item_transactions as b where a.transactionId = b.transactionId")
+    sqlContext.sql("insert into table retail_products partition(productSubCategory) select productId, productCategory,  manufacturer, productName, price, productSubCategory from phoenix_retail_products")
   }
 }
